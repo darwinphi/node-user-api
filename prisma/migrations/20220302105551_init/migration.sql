e15134d45d74e20fafc3c983f0d1a7ce919bd60f
Darwin Manalo <darwinmanalophi@gmail.com>
2022-03-02 18:58:56 +0800

    Add seed, reset migration

--- prisma/migrations/20220302105551_init/migration.sql
+++ prisma/migrations/20220302105551_init/migration.sql
@@ -11,6 +11,7 @@ CREATE TABLE `users` (
     `password` VARCHAR(255) NOT NULL,
     `is_admin` BOOLEAN NOT NULL,
 
+    UNIQUE INDEX `users_email_key`(`email`),
     UNIQUE INDEX `users_username_key`(`username`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
---
+++ prisma/seed.js
@@ -0,0 +1,57 @@
+import pkg from "@prisma/client";
+const { PrismaClient } = pkg;
+const prisma = new PrismaClient();
+
+const users = [
+  {
+    email: "admin@email.com",
+    first_name: "Darwin",
+    last_name: "Manalo",
+    address: "Paranaque City",
+    postcode: "1700",
+    contact_number: "092626262626",
+    email: "admin@email.com",
+    username: "admin",
+    password: "secret",
+    is_admin: true,
+  },
+  {
+    email: "member@email.com",
+    first_name: "John",
+    last_name: "Doe",
+    address: "Manila",
+    postcode: "1500",
+    contact_number: "091616161616",
+    email: "member@email.com",
+    username: "member",
+    password: "secret",
+    is_admin: false,
+  },
+];
+
+users.forEach(async (user) => {
+  try {
+    await prisma.user.upsert({
+      where: { email: user.email },
+      update: {},
+      create: {
+        first_name: user.first_name,
+        last_name: user.last_name,
+        address: user.address,
+        postcode: user.postcode,
+        contact_number: user.contact_number,
+        email: user.email,
+        username: user.username,
+        password: user.password,
+        is_admin: user.is_admin,
+      },
+    });
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  } finally {
+    prisma.$disconnect();
+  }
+});
+
+console.log(users);
