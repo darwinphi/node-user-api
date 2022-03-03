ba12a86b6e703a78d48cc23aea1ecfabe1d1e78a
Darwin Manalo <darwinmanalophi@gmail.com>
2022-03-03 22:16:53 +0800

    Add create user form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
   return (
     <main>
       <section>
-        <h1>Users</h1>
+        <h1>👨‍💻 Users</h1>
         <table>
           <thead>
             <tr>
@@ -50,7 +50,35 @@ function App() {
               ))}
           </tbody>
         </table>
-        <ul></ul>
+        <form>
+          <h1>Create User</h1>
+          <p>First Name</p>
+
+          <input type="text" name="firstName" />
+          <p>Last Name</p>
+          <input type="text" name="lastName" />
+
+          <p>Username </p>
+          <input type="text" name="userName" />
+
+          <p>Address </p>
+          <input type="text" name="address" />
+
+          <p>Postcode </p>
+          <input type="text" name="postcode" />
+
+          <p>Contact No </p>
+          <input type="text" name="contactNumber" />
+
+          <p>Email </p>
+          <input type="text" name="email" />
+
+          <p>
+            <button type="submit" value="Submit">
+              💾 Save
+            </button>
+          </p>
+        </form>
       </section>
     </main>
   );
