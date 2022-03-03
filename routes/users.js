5d661925d43427c9ac899051efa111c2437cc067
Darwin Manalo <darwinmanalophi@gmail.com>
2022-03-03 11:42:25 +0800

    Initial /users/create api and test

--- routes/users.js
+++ routes/users.js
@@ -1,37 +1,37 @@
 import express from "express";
 const router = express.Router();
-import { getAllUsers } from "../repo/user.js";
+import { getAllUsers, createUser } from "../repo/user.js";
 
 router.get("/", async (_req, res) => {
   const users = await getAllUsers();
   res.json({ users });
 });
 
-// router.post("/create", async (req, res) => {
-//   const {
-//     email,
-//     first_name,
-//     last_name,
-//     address,
-//     postcode,
-//     contact_number,
-//     username,
-//     password,
-//   } = req.body;
-//   const newUser = await user.create({
-//     data: {
-//       email,
-//       first_name,
-//       last_name,
-//       address,
-//       postcode,
-//       contact_number,
-//       username,
-//       password,
-//     },
-//   });
+router.post("/create", async (req, res) => {
+  const {
+    email,
+    firstName,
+    lastName,
+    address,
+    postcode,
+    contactNumber,
+    username,
+    password,
+    isAdmin,
+  } = req.body;
+  const newUser = createUser({
+    email,
+    firstName,
+    lastName,
+    address,
+    postcode,
+    contactNumber,
+    username,
+    password,
+    isAdmin,
+  });
 
-//   res.json(newUser);
-// });
+  res.status(201).json({ newUser });
+});
 
 export default router;
--- tests/user.test.js
+++ tests/user.test.js
@@ -19,32 +19,57 @@ const newUser = {
 };
 
 beforeAll(async () => {
+  await user.deleteMany();
   const createdUser = await createUser(newUser);
   console.log("✨ 1 user successfully created!", createdUser);
 });
 
 afterAll(async () => {
-  await user.deleteMany();
+  await user.deleteMany({});
 });
 
-it("responds with json", async () => {
-  const response = await request(app)
-    .get("/users")
-    .set("Accept", "application/json");
+describe("/users", () => {
+  it("responds a json with the list of users", async () => {
+    const response = await request(app)
+      .get("/users")
+      .set("Accept", "application/json");
 
-  expect(response.headers["content-type"]).toMatch(/json/);
-  expect(response.status).toEqual(200);
-  expect(response.body).toEqual({
-    users: [
-      {
+    expect(response.headers["content-type"]).toMatch(/json/);
+    expect(response.status).toEqual(200);
+    expect(response.body).toEqual({
+      users: [
+        {
+          address: "Makati",
+          contact_number: "092626262626",
+          email: "jane@email.com",
+          first_name: "Jane",
+          last_name: "Doe",
+          postcode: "1200",
+          username: "jane",
+        },
+      ],
+    });
+  });
+});
+
+describe("/users/create", () => {
+  it("response a json with the created user", async () => {
+    const response = await request(app)
+      .post("/users/create")
+      .send({
+        email: "john@email.com",
+        firstName: "John",
+        lastName: "Doe",
         address: "Makati",
-        contact_number: "092626262626",
-        email: "jane@email.com",
-        first_name: "Jane",
-        last_name: "Doe",
         postcode: "1200",
-        username: "jane",
-      },
-    ],
+        contactNumber: "092626262626",
+        username: "john",
+        password: "secret",
+        isAdmin: false,
+      })
+      .set("Accept", "application/json");
+
+    expect(response.headers["content-type"]).toMatch(/json/);
+    expect(response.status).toEqual(201);
   });
 });
