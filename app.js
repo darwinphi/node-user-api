93aa1097abfc32cd7a42fd1e6c605a33427f2334
Darwin Manalo <darwinmanalophi@gmail.com>
2022-03-03 21:28:50 +0800

    Add cors, Fix client path

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ import express from "express";
 const app = express();
 import authRouter from "./routes/auth.js";
 import usersRouter from "./routes/users.js";
+import path from "path";
+import cors from "cors";
+const __dirname = path.resolve();
 
+app.use(cors());
 app.use(express.json());
+app.use(express.static(path.resolve(__dirname, "./client/dist")));
 app.use("/auth", authRouter);
 app.use("/users", usersRouter);
 
