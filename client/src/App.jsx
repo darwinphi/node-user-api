625c41a62314e38193b0314f65afe1e9640e6001
Darwin Manalo <darwinmanalophi@gmail.com>
2022-03-03 21:55:47 +0800

    Add users table

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
   const [users, setUsers] = useState(null);
@@ -13,14 +14,44 @@ function App() {
 
   return (
     <main>
-      <ul>
-        {users &&
-          users.map((user, i) => (
-            <li key={i}>
-              {user.first_name}, {user.last_name}
-            </li>
-          ))}
-      </ul>
+      <section>
+        <h1>Users</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Username</th>
+              <th>Address</th>
+              <th>Postcode</th>
+              <th>Contact No.</th>
+              <th>Email</th>
+              <th colSpan={2}>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users &&
+              users.map((user, i) => (
+                <tr key={i}>
+                  <td>
+                    {user.first_name}, {user.last_name}
+                  </td>
+                  <td>{user.username}</td>
+                  <td>{user.address}</td>
+                  <td>{user.postcode}</td>
+                  <td>{user.contact_number}</td>
+                  <td>{user.email}</td>
+                  <td>
+                    <button>Edit</button>
+                  </td>
+                  <td>
+                    <button>Delete</button>
+                  </td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+        <ul></ul>
+      </section>
     </main>
   );
 }
