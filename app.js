8182316c7589d2982f3393e486233a516265548f
Darwin Manalo <darwinmanalophi@gmail.com>
2022-03-04 22:00:47 +0800

    Remove comments

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ import express from "express";
 const app = express();
 import authRouter from "./routes/auth.js";
 import usersRouter from "./routes/users.js";
-import authMiddleware from "./middleware/auth.js";
 import path from "path";
 import cors from "cors";
 const __dirname = path.resolve();
@@ -10,9 +9,6 @@ const __dirname = path.resolve();
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, "./client/dist")));
-// app.get("/", authMiddleware, (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "./client/dist", "index.html"));
-// });
 app.use("/auth", authRouter);
 app.use("/users", usersRouter);
 
